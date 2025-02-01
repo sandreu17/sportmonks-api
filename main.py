b7eb587049a60c61bf57d21d3cce085db4124ebb
sandreu17 <sandreuo@gmail.com>
2025-02-01 09:23:00 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Query
+import requests
+
+app = FastAPI()
+
+SPORTMONKS_API_KEY = "c5Axt3Jxa4GRd82UUbHLIjhE4xeqQMinHdceCUaBos6rjessx40YHc50RXQk"
+BASE_URL = "https://api.sportmonks.com/v3/football"
+
+@app.get("/get_league_id/")
+def get_league_id(league_name: str):
+    url = f"{BASE_URL}/leagues/search/{league_name}"
+    params = {"api_token": SPORTMONKS_API_KEY}
+    response = requests.get(url, params=params)
+    return response.json()
+
+@app.get("/get_fixture/")
+def get_fixture(match_date: str):
+    url = f"{BASE_URL}/fixtures/date/{match_date}"
+    params = {"api_token": SPORTMONKS_API_KEY}
+    response = requests.get(url, params=params)
+    return response.json()
